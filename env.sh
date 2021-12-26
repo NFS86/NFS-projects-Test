28ffa48b5f4d1419dc94cabf16174bda0ec8e72f
NFS86 <jarbull86@gmail.com>
2021-12-26 14:39:54 +0700

    anu
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- env.sh
+++ env.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 cd /tmp
-export DEBIAN_FRONTEND=noninteractive
-export ALLOW_MISSING_DEPENDENCIES=true
 mkdir -p ~/.config/rclone
 echo "$rcloneconfig" > ~/.config/rclone/rclone.conf
-DEBIAN_FRONTEND=noninteractive
-sudo ln -sf /usr/bin/python3 /usr/bin/python
-git config --global user.name "NFS86"
-git config --global user.email "jarbull86@gmail.com"
 mkdir -p /tmp/ccache
 rclone copy anggitjav:ccache/rom/ccache.tar.gz /tmp -P
 time tar xf ccache.tar.gz
--- pondasi/collect.sh
+++ pondasi/collect.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 cd /tmp/rom
 
-. build/envsetup.sh
-lunch spark_rosy-userdebug
-curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===========================%0A<code>Building ccache started..</code>%0A$(echo "${var_cache_report_config}")"
-export CCACHE_DIR=/tmp/ccache
-export CCACHE_EXEC=$(which ccache)
-export USE_CCACHE=1
+function setcache() {
 ccache -M 20G
 ccache -o compression=true
 ccache -z
-export ALLOW_MISSING_DEPENDENCIES=true
-export TZ=Asia/Jakarta
-export BUILD_USERNAME=finix
-export BUILD_HOSTNAME=rosy
+}
+
+function finerr() {
+    curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="================================%0A<code>Building Rom Gagal,Jiancoeg..</code>%0A================================" \
+    curl -s -X POST https://api.telegram.org/$TG_TOKEN/sendSticker -d sticker=CAACAgIAAx0CXjGT1gACDRRhYsUKSwZJQFzmR6eKz2aP30iKqQACPgADr8ZRGiaKo_SrpcJQIQQ -d chat_id=$TG_CHAT_ID
+    exit 1
+}
+
+function buildenvsetup() {
+. build/envsetup.sh
+}
+
+function lunch() {
+lunch spark_rosy-userdebug
+}
+
+function build() {
+curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===========================%0A<code>Building ccache started..</code>%0A$(echo "${var_cache_report_config}")"
+export CCACHE_EXEC=$(which ccache)
 export WITH_GAPPS=false
-make SYSTEMIMAGE &
-sleep 90m
+make -j8 systemimage &
+sleep 95m
 kill %1
 ccache -s
+}
+setcache
+buildenvsetup
+lunch
+build
