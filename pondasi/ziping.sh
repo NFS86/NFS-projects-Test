659cd4c1e65e022a49443b9c43ebffc4bfbe984d
NFS86 <jarbull86@gmail.com>
2021-12-26 07:27:06 +0700

    puyeng
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- pondasi/ziping.sh
+++ pondasi/ziping.sh
@@ -2,16 +2,15 @@
 
 cd /tmp
 
-curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Compres ccache"
 com ()
 {
     tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
 }
 
 time com ccache 1
-time com rom 1
-
-cd /tmp
-curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading ccache...."
 rclone copy ccache.tar.gz anggitjav:ccache/rom -P
+rm -rf rom.tar.gz 
+time com rom 1
 rclone copy rom.tar.gz anggitjav:Spark -P
+rm -rf rom.tar.gz
+curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Build ccache done...."
