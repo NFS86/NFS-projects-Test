e7d1695d1d4e421d51d68450cac4c1c09d42d46a
NFS86 <jarbull86@gmail.com>
2021-12-27 23:41:43 +0700

    zzz
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- env.sh
+++ env.sh
@@ -4,9 +4,11 @@ cd /tmp
 mkdir -p ~/.config/rclone
 echo "$rcloneconfig" > ~/.config/rclone/rclone.conf
 mkdir -p /tmp/ccache
-rclone copy NFS:ccache/rom/ccache.tar.gz /tmp -P
-time tar xf ccache.tar.gz
-rm -rf ccache.tar.gz
+#rclone copy NFS:ccache/rom/ccache.tar.gz /tmp -P
+#time tar xf ccache.tar.gz
+#rm -rf ccache.tar.gz
 cat /etc/os*
 env
 nproc
+gcc -version
+clang --version
--- pondasi/ziping.sh
+++ pondasi/ziping.sh
@@ -8,7 +8,7 @@ com ()
 }
 
 time com ccache 1
-rclone copy ccache.tar.gz anggitjav:ccache/rom -P
+rclone copy ccache.tar.gz NFS:ccache/rom -P
 rm -rf ccache.tar.gz 
 
 curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Build ccache done...."
