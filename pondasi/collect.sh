296b0349a26e9ae53cb418ac18a56296e30f09da
NFS86 <jarbull86@gmail.com>
2021-12-28 17:40:26 +0700

    puyeng
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- pondasi/collect.sh
+++ pondasi/collect.sh
@@ -22,10 +22,10 @@ lunch octavi_rosy-userdebug
 }
 
 function build() {
-curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===========================%0A<code>Building ccache started..</code>%0A$(echo "${var_cache_report_config}")"
 export CCACHE_EXEC=$(which ccache)
-make packages/apps/Settings -j8
-#sleep 95m
+brunch rosy &
+sleep 95m
+finerr
 kill %1
 ccache -s
 }
