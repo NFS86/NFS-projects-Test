15d937fbd2e743e3fecb56d21df20b705eb1a77b
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2021-12-29 13:00:57 +0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -2,5 +2,5 @@
 
 cd /tmp/rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS86/local_manifest --depth 1 -b OctaviOS-12 .repo/local_manifests
+git clone https://github.com/NFS86/local_manifest --depth 1 -b Test .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
