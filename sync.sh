0cd513cf2342e90080869ec50421486a6044ec84
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-01-01 15:50:23 +0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -3,4 +3,4 @@
 cd /tmp/rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/NFS86/local_manifest --depth 1 -b PE_12 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
