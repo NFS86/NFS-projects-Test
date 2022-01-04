7dd7b4c3e781d64a393abac9cfae5dee104904f3
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-01-04 21:03:10 +0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /tmp/rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS86/local_manifest --depth 1 -b PE_12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NFS86/local_manifest --depth 1 -b oct-12 .repo/local_manifests
 repo sync -c -j8 --force-sync --no-clone-bundle --no-tags
