4d7bd69e95ff81a0938c323e0961ad4b3766f249
NFS86 <jarbull86@gmail.com>
2022-01-09 08:27:29 +0700

    nguwawor
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- env.sh
+++ env.sh
@@ -7,8 +7,15 @@ mkdir -p /tmp/ccache
 rclone copy NFS:ccache/rom/ccache.tar.gz /tmp -P
 time tar xf ccache.tar.gz
 rm -rf ccache.tar.gz
+
+mkdir -p /tmp/rom
+rclone copy NFS:ccache/rom/out.tar.gz /tmp/rom -P
+cd /tmp/rom
+time tar xf out.tar.gz
+rm -rf out.tar.gz
 cat /etc/os*
 env
 nproc
 gcc --version
 clang --version
+cd /tmp
