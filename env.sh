c59736cc448ad009dcff4881bda5e6fc3afe725b
NFS86 <jarbull86@gmail.com>
2022-01-09 10:55:08 +0700

    anu iki
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- env.sh
+++ env.sh
@@ -4,12 +4,12 @@ cd /tmp
 mkdir -p ~/.config/rclone
 echo "$rcloneconfig" > ~/.config/rclone/rclone.conf
 mkdir -p /tmp/ccache
-rclone copy NFS:ccache/rom/ccache.tar.gz /tmp -P
+rclone copy NFS:ccache/arcanaos/ccache.tar.gz /tmp -P
 time tar xf ccache.tar.gz
 rm -rf ccache.tar.gz
 
 mkdir -p /tmp/rom
-rclone copy NFS:ccache/rom/out.tar.gz /tmp/rom -P
+rclone copy NFS:ccache/arcanaos/out.tar.gz /tmp/rom -P
 cd /tmp/rom
 time tar xf out.tar.gz
 rm -rf out.tar.gz
--- ziping.sh
+++ ziping.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 
+function pushcache() {
 cd /tmp
-
 com ()
 {
     tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
 }
-
 time com ccache 1
-rclone copy ccache.tar.gz NFS:ccache/rom -P
+rclone copy ccache.tar.gz NFS:ccache/arcanaos -P
 rm -rf ccache.tar.gz
+}
 
+function pushout() {
 cd /tmp/rom
-
 com ()
 {
     tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
 }
-
 time com out 1
-rclone copy out.tar.gz NFS:ccache/rom -P
+rclone copy out.tar.gz NFS:ccache/arcanaos -P
 rm -rf out.tar.gz
+}
 
+function pushrom() {
+cd /tmp
+com ()
+{
+    tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
+}
+time com rom 1
+rclone copy rom.tar.gz NFS:ccache/arcanaos -P
+rm -rf rom.tar.gz
+}
+
+function dono() {
 curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Build ccache done...."
+}
+
+pushcache
+pushout
+#pushrom
+dono
