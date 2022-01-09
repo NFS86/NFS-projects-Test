a5e495417ddd64b247b5d10aa2d3d6dcac3989a9
NFS86 <jarbull86@gmail.com>
2022-01-09 21:00:15 +0700

    xxx
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- collect.sh
+++ collect.sh
@@ -2,7 +2,7 @@
 cd /tmp/rom
 
 function setcache() {
-ccache -M 20G
+ccache -M 50G
 ccache -o compression=false
 ccache -z
 }
@@ -31,7 +31,7 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 
 function build() {
 make bacon -j8 &
-sleep 35m
+sleep 40m
 kill %1
 ccache -s
 }
