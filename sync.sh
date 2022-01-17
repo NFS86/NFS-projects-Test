1ba388f9db7173acc7bd0965b07ac303c9535d6f
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-01-17 17:10:56 +0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -2,5 +2,5 @@
 
 cd /tmp/rom
 repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-projects/local_manifest --depth 1 -b PE-12 .repo/local_manifests
+git clone https://github.com/NFS-projects/local_manifest --depth 1 -b arcana-12 .repo/local_manifests
 repo sync -c -j8 --force-sync --no-clone-bundle --no-tags
