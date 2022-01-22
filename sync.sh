379e57c74f1a0367fe14ebb9c3a44e8a6d39fcd8
NFS86 <jarbull86@gmail.com>
2022-01-23 06:00:07 +0700

    Build witch all proc
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /tmp/rom
-repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-projects/local_manifest --depth 1 -b arcana-12 .repo/local_manifests
-repo sync -c -j8 --force-sync --no-clone-bundle --no-tags
+repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x
+git clone https://github.com/NFS-projects/local_manifest --depth 1 -b PE-12 .repo/local_manifests
+repo sync -j$(nproc --all) -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
