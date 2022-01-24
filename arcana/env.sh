60071a0e7ac16bb9a169eeab6e34f1eb4c75114b
NFS86 <jarbull86@gmail.com>
2022-01-24 14:52:18 +0700

    test
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- arcana/env.sh
+++ arcana/env.sh
@@ -4,23 +4,23 @@ cd /tmp
 mkdir -p ~/.config/rclone
 echo "$rcloneconfig" > ~/.config/rclone/rclone.conf
 
-ifeq ($BUILD_SYSTEM_ONLY:true)
+if [ "$BUILD_SYSTEM_ONLY" == "true" ]; then
   echo Build variant SYSTEM terdeteksi..
   echo Melanjutkan untuk mengambil ccache SYSTEM
   mkdir -p /tmp/ccache
   rclone copy NFS:ccache/arcanaos/system/ccache.tar.gz /tmp -P
   time tar xf ccache.tar.gz
   rm -rf ccache.tar.gz
-endif
+fi
 
-ifeq ($BUILD_VENDOR_ONLY:true)
+if [ "$BUILD_VENDOR_ONLY" == "true" ]; then
   echo Build variant VENDOR terdeteksi..
   echo Melanjutkan untuk mengambil ccache VENDOR
   mkdir -p /tmp/ccache
   rclone copy NFS:ccache/arcanaos/vendor/ccache.tar.gz /tmp -P
   time tar xf ccache.tar.gz
   rm -rf ccache.tar.gz
-endif
+fi
 
 function check() {
 cat /etc/os*
