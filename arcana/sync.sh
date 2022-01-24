748c2ff73458fe5796c951c84663215ba575ad75
NFS86 <jarbull86@gmail.com>
2022-01-24 15:05:17 +0700

    Test full env
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- arcana/sync.sh
+++ arcana/sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /tmp/rom
-repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-projects/local_manifest --depth 1 -b arcana-12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u $SYNC -g default,-mips,-darwin,-notdefault
+git clone $LOCAL_MANIFEST --depth 1 .repo/local_manifests
 repo sync -j8 -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
