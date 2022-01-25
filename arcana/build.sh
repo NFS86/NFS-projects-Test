8c24aa66b6b3c0ac3cae8b73ef49eeab90dc6a00
NFS86 <jarbull86@gmail.com>
2022-01-26 01:16:57 +0700

    Test
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- arcana/build.sh
+++ arcana/build.sh
@@ -16,8 +16,15 @@ lunch $LUNCH
 }
 
 function build() {
-curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="================================%0A<code>Test Building image..</code>%0A$(echo "${var_cache_report_config}")"
-curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="================================%0A<b>Show build: </b>https://cirrus-ci.com/github/NFS86/NFS-projects-Test%0A================================"
+ccache -M 20G
+export USE_CCACHE=1
+export CCACHE_DIR=/tmp/ccache
+export CCACHE_EXEC=$(which ccache)
+ccache -o compression=true
+ccache - a fast C/C++ compiler cache
+for t in ccache gcc g++ cc c++ clang clang++; do ln -vs /usr/bin/ccache /usr/local/bin/$t; done
+ccache -z
+cp -r /tmp/ccache/ccache.conf /etc/ccache.conf
 make $BUILD_TYPE -j8 &
 sleep 95m
 kill %1
