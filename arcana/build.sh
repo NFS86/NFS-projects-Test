aaaa23825743e171bf8fc2c3e024b470dd53991c
NFS86 <jarbull86@gmail.com>
2022-01-27 02:48:58 +0700

    ngent
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- arcana/build.sh
+++ arcana/build.sh
@@ -39,7 +39,7 @@ if [ "$BUILD_CCACHE_ONLY" == "true" ]; then
   cp -r /tmp/ccache/ccache.conf /etc/ccache.conf
   . build/envsetup.sh
   lunch $LUNCH
-  $BUILD_TYPE -j25 &
+  $BUILD_TYPE &
   sleep 95m
   kill %1
   ccache -s
@@ -58,7 +58,7 @@ if [ "$BUILD_CCACHE_ONLY" == "false" ]; then
   cp -r /tmp/ccache/ccache.conf /etc/ccache.conf
   . build/envsetup.sh
   lunch $LUNCH
-  $BUILD_TYPE -j25
+  $BUILD_TYPE
   ccache -s
 fi
 
