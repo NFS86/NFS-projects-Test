8aacba7aa3a653a8b79e235e905c8edf27c7b87e
NFS86 <jarbull86@gmail.com>
2022-01-26 10:33:15 +0700

    Add auto detect build ccache only (1/1)
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- arcana/build.sh
+++ arcana/build.sh
@@ -1,36 +1,6 @@
 #!/bin/bash
 cd /tmp/rom
 
-function finerr() {
-    curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="================================%0A<code>Building Gagal,Jiancoeg..</code>%0A================================" \
-    curl -s -X POST https://api.telegram.org/$TG_TOKEN/sendSticker -d sticker=CAACAgIAAx0CXjGT1gACDRRhYsUKSwZJQFzmR6eKz2aP30iKqQACPgADr8ZRGiaKo_SrpcJQIQQ -d chat_id=$TG_CHAT_ID
-    exit 1
-}
-
-function sentup() {
-. build/envsetup.sh
-}
-
-function anu() {
-lunch $LUNCH
-}
-
-function build() {
-ccache -M 20G
-export USE_CCACHE=1
-export CCACHE_DIR=/tmp/ccache
-export CCACHE_EXEC=$(which ccache)
-ccache -o compression=true
-ccache - a fast C/C++ compiler cache
-for t in ccache gcc g++ cc c++ clang clang++; do ln -vs /usr/bin/ccache /usr/local/bin/$t; done
-ccache -z
-cp -r /tmp/ccache/ccache.conf /etc/ccache.conf
-make $BUILD_TYPE -j8 &
-sleep 95m
-kill %1
-ccache -s
-}
-
 function check() {
 if ! [ -a "$SYSTEM_IMAGE" ]; then
 	checkv
@@ -50,7 +20,44 @@ if ! [ -a "$BOOT_IMAGE" ]; then
 fi
 }
 
-sentup
-anu
-build
+function finerr() {
+    curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="================================%0A<code>Building Gagal,Jiancoeg..</code>%0A================================" \
+    curl -s -X POST https://api.telegram.org/$TG_TOKEN/sendSticker -d sticker=CAACAgIAAx0CXjGT1gACDRRhYsUKSwZJQFzmR6eKz2aP30iKqQACPgADr8ZRGiaKo_SrpcJQIQQ -d chat_id=$TG_CHAT_ID
+    exit 1
+}
+
+if [ "$BUILD_CCACHE_ONLY" == "true" ]; then
+  ccache -M 20G
+  export USE_CCACHE=1
+  export CCACHE_DIR=/tmp/ccache
+  export CCACHE_EXEC=$(which ccache)
+  ccache -o compression=true
+  ccache - a fast C/C++ compiler cache
+  for t in ccache gcc g++ cc c++ clang clang++; do ln -vs /usr/bin/ccache /usr/local/bin/$t; done
+  ccache -z
+  cp -r /tmp/ccache/ccache.conf /etc/ccache.conf
+  . build/envsetup.sh
+  lunch $LUNCH
+  make $BUILD_TYPE -j8 &
+  sleep 95m
+  kill %1
+  ccache -s
+fi
+
+if [ "$BUILD_CCACHE_ONLY" == "false" ]; then
+  ccache -M 20G
+  export USE_CCACHE=1
+  export CCACHE_DIR=/tmp/ccache
+  export CCACHE_EXEC=$(which ccache)
+  ccache -o compression=true
+  ccache - a fast C/C++ compiler cache
+  for t in ccache gcc g++ cc c++ clang clang++; do ln -vs /usr/bin/ccache /usr/local/bin/$t; done
+  ccache -z
+  cp -r /tmp/ccache/ccache.conf /etc/ccache.conf
+  . build/envsetup.sh
+  lunch $LUNCH
+  make $BUILD_TYPE -j8
+  ccache -s
+fi
+
 #check
