41b5d7eb972dc80ec283df1b24a075cefc08db57
NFS86 <jarbull86@gmail.com>
2022-01-29 05:49:56 +0700

    angel
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- script/build.sh
+++ script/build.sh
@@ -52,7 +52,7 @@ if [ "$BUILD_CCACHE_ONLY" == "true" ]; then
   sed -i '78 i \\t"ccache":  Allowed,' build/soong/ui/build/paths/config.go
   . build/envsetup.sh
   lunch $LUNCH
-  $BUILD_TYPE -j$(nproc --all) &
+  $BUILD_TYPE -j23 &
   if [ "$BUILD_OUT_FOLDER" == "yes" ]; then
      echo BUILD OUT FOLDER AKTIF..
      sleep 80m
@@ -81,7 +81,7 @@ if [ "$BUILD_CCACHE_ONLY" == "false" ]; then
   sed -i '78 i \\t"ccache":  Allowed,' build/soong/ui/build/paths/config.go
   . build/envsetup.sh
   lunch $LUNCH
-  $BUILD_TYPE -j$(nproc --all)
+  $BUILD_TYPE -j23
   check
   ccache -s
 fi
--- script/sync.sh
+++ script/sync.sh
@@ -15,4 +15,4 @@ ccache -z
 cd /tmp/cirrus-ci-build/rom
 repo init --depth=1 --no-repo-verify -u $SYNC -g default,-mips,-darwin,-notdefault
 git clone $LOCAL_MANIFEST --depth 1 .repo/local_manifests
-repo sync -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j$(nproc --all)
+repo sync -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j8
