874727b97c9d9a8160ab859a23812581db88db1a
NFS86 <jarbull86@gmail.com>
2022-01-29 02:57:48 +0700

    Anu
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- script/sync.sh
+++ script/sync.sh
@@ -15,4 +15,4 @@ ccache -z
 cd /tmp/cirrus-ci-build/rom
 repo init --depth=1 --no-repo-verify -u $SYNC -g default,-mips,-darwin,-notdefault
 git clone $LOCAL_MANIFEST --depth 1 .repo/local_manifests
-repo sync -j8 -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
+repo sync -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j8
