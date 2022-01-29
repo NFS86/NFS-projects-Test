ba0eba8038bccaca8960df2ef7266b94bdef812b
NFS86 <jarbull86@gmail.com>
2022-01-30 05:48:13 +0700

    taex
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- script/sync.sh
+++ script/sync.sh
@@ -12,6 +12,6 @@ ccache - a fast C/C++ compiler cache
 for t in ccache gcc g++ cc c++ clang clang++; do ln -vs /usr/bin/ccache /usr/local/bin/$t; done
 ccache -z
 cd /tmp/cirrus-ci-build/rom
-repo init --depth=1 --no-repo-verify -u $SYNC -g default,-mips,-linux,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u $SYNC -g default,-mips,-darwin,-notdefault
 git clone $LOCAL_MANIFEST --depth 1 .repo/local_manifests
 repo sync -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j8
