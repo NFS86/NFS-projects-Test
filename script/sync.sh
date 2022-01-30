19b15df8fed59525422bb39150e846c636708db7
NFS86 <jarbull86@gmail.com>
2022-01-31 00:13:42 +0700

    capek
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- script/sync.sh
+++ script/sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /tmp/cirrus-ci-build/rom
-repo init --depth=1 --no-repo-verify -u $SYNC -g default,-mips,-darwin,-notdefault
+repo init -q --no-repo-verify --depth=1 -u $SYNC -g default,-device,-mips,-darwin,-notdefault
 git clone $LOCAL_MANIFEST --depth 1 .repo/local_manifests
-repo sync -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
