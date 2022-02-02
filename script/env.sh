a57e8e1745cfc86bffabe28a3b98029ced7c581f
NFS86 <jarbull86@gmail.com>
2022-02-03 06:17:44 +0700

    fix restore ccache
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- script/env.sh
+++ script/env.sh
@@ -3,9 +3,8 @@
 cd /tmp/cirrus-ci-build
 mkdir -p ~/.config/rclone
 echo "$rcloneconfig" > ~/.config/rclone/rclone.conf
-
 mkdir -p /tmp/cirrus-ci-build/ccache
-rclone copy NFS:ccache/$DIR/system/ccache.tar.gz /tmp/cirrus-ci-build -P
+rclone copy NFS:ccache/$DIR/ccache.tar.gz /tmp/cirrus-ci-build -P
 time tar xf ccache.tar.gz
 rm -rf ccache.tar.gz
 cat /etc/os*
