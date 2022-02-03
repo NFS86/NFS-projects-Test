923f9ca1a6603a9bb8f6eb070e693e0e8098da2c
NFS86 <jarbull86@gmail.com>
2022-02-03 13:43:41 +0700

    ccache no limit
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- script/build.sh
+++ script/build.sh
@@ -6,8 +6,8 @@ export CCACHE_DIR=/tmp/cirrus-ci-build/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 export CCACHE_COMPRESS=true
-ccache -M 50G
-ccache -F 999999999
+ccache -M 0
+ccache -F 0
 ccache - a fast C/C++ compiler cache
 for t in ccache gcc g++ cc c++ clang clang++; do ln -vs /usr/bin/ccache /usr/local/bin/$t; done   
 ccache -z
