cd717c869a79bf2a0406cc43941b86fdaebba1a5
NFS86 <jarbull86@gmail.com>
2022-02-03 16:23:24 +0700

    ccache regenrate
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- script/build.sh
+++ script/build.sh
@@ -9,12 +9,14 @@ export CCACHE_COMPRESS=true
 ccache -M 0
 ccache -F 0
 ccache - a fast C/C++ compiler cache
-ccache -z
+ccache -C && ccache -z
 
 if [ "$BUILD_CCACHE_ONLY" == "true" ]; then
   . build/envsetup.sh
   lunch $LUNCH
-  $BUILD_TYPE -j10 &
+  $BUILD_TYPE -j8 &
+  sleep 40m
+  $BUILD_TYPE -j8 &
   sleep 95m
   kill %1
   ccache -x && ccache -s
@@ -23,6 +25,6 @@ fi
 if [ "$BUILD_CCACHE_ONLY" == "false" ]; then
   . build/envsetup.sh
   lunch $LUNCH
-  $BUILD_TYPE -j10
+  $BUILD_TYPE -j8
   ccache -x && ccache -s
 fi
