f8ce2d40212a55f661a5bd1bd7fab6ad139b5344
NFS86 <jarbull86@gmail.com>
2022-02-04 17:21:20 +0700

    Final
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- script/ziping.sh
+++ script/ziping.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-cd /tmp/cirrus-ci-build
-com ()
-{
-    tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
-}
-time com ccache 1
-rclone copy ccache.tar.gz NFS:ccache/$DIR -P
-rm -rf ccache.tar.gz
+if [ "$BUILD_CCACHE_ONLY" == "true" ]; then
+   cd /tmp/cirrus-ci-build
+   com ()
+   {
+       tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
+   }
+   time com ccache 1
+   rclone copy ccache.tar.gz NFS:ccache/$DIR -P
+   rm -rf ccache.tar.gz
+fi
+
+if [ "$BUILD_CCACHE_ONLY" == "false" ]; then
+   rclone copy /tmp/cirrus-ci-build/and out/target/product/$device/$rom_name_$device*.zip NFS:rom/$DIR -P
+fi
