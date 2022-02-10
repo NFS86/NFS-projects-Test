098bb9f619875b6505f1c1209ec428889ace96fb
NFS86 <jarbull86@gmail.com>
2022-02-10 19:05:01 +0700

    try sync witch j4
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- script/sync.sh
+++ script/sync.sh
@@ -3,4 +3,4 @@
 cd /tmp/cirrus-ci-build/rom
 repo init --depth=1 --no-repo-verify -u $SYNC -g default,-mips,-darwin,-notdefault
 git clone $LOCAL_MANIFEST --depth 1 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
