6616524cf4e4cf5cea3ec167f1ec4de888f4400f
NFS86 <jarbull86@gmail.com>
2022-02-15 20:31:30 +0700

    Hack CPU

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,,-mips,-darwin,-notdefault
 git clone https://github.com/NFS-projects/local_manifest -b evolution-12 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --force-sync -j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --force-sync -j$CIRRUS_CPU
 
 # build rom
 source build/envsetup.sh
