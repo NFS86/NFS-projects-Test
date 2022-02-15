8142e21cac349df5faa7219e23e1a513bf8e3ff6
NFS86 <jarbull86@gmail.com>
2022-02-15 19:52:08 +0700

    Test fix sync (2)
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,,-mips,-darwin,-notdefault
 git clone https://github.com/NFS-projects/local_manifest -b evolution-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --force-sync -j$(nproc --all)
 
