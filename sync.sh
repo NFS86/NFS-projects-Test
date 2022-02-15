a31db2895101db33c58e1979ab60044d56b98b22
NFS86 <jarbull86@gmail.com>
2022-02-15 20:21:14 +0700

    Angel
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,,-mips,-darwin,-notdefault
 git clone https://github.com/NFS-projects/local_manifest -b evolution-12 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --force-sync -j7
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
